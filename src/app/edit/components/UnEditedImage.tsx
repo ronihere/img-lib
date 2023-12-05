4059a7d37e26fe22273930af4ab8daf106b4d0fc
ronihere <roni78rm@gmail.com>
2023-12-05 19:04:32 +0530

    changed the BG of UneditedImage component

--- src/app/edit/components/UnEditedImage.tsx
+++ src/app/edit/components/UnEditedImage.tsx
@@ -1,6 +1,6 @@
 export default function UnEditedImage() {
   return (
-      <div className="w-full h-96 bg-gray-50 text-black flex items-center justify-center">
+      <div className="w-full h-96 bg-gray-850 border flex items-center justify-center">
           Apply some Filter
     </div>
   )
